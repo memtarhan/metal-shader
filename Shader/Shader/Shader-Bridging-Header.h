3a68346d0f7c7facd0833f98997e1e67f696433c
Mehmet Tarhan <memtarhan@gmail.com>
2020-09-09 23:10:02 +0300

    Created project and added metal shader

---
+++ Shader/Shader/Shader-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Shader-Bridging-Header.h
+//  Shader
+//
+//  Created by Mehmet Tarhan on 9.09.2020.
+//  Copyright © 2020 Mehmet Tarhan. All rights reserved.
+//
+
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "ShaderTypes.h"
